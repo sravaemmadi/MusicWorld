8408f7f97e00ae38c29598eed5cd59af609837cd
Sravan Emmadi <sra1tyson@gmail.com>
2025-02-10 18:00:34 -0600

    changed scr to src in index and transition time in style.css added audio elemtn in progressing the play and pause change let audioElement with song from masterplay

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 console.log("Welcome to Spotify");
 
 let SongINdex = 0;
-let audioElement = new Audio('masterPlay');
+let audioElement = new Audio('Songs/[iSongs.info] 01 - Naa Favourite Naa Pellam.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgessBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
 let song = [
     {songName:"Naa Favourite", filePath: "Songs/[iSongs.info] 01 - Naa Favourite Naa Pellam.mp3", coverPath: "cover1/pelama.jpeg"},
     {songName:"New york", filePath: "Songs/03 - Newyork Nagaram  - SenSongsMp3.co.mp3", coverPath: "cover1/new york.jpg"},
@@ -13,7 +14,22 @@ let song = [
     {songName:"Attantode", filePath: "Songs/Oosupodu-SenSongsMp3.Co.mp3", coverPath: "cover1/fida.jpg"},
     {songName:"Attantode", filePath: "Songs/Peelings.mp3", coverPath: "cover1/peelings.jpg"},
 ]
-
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime <= 0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    } else {
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+});
 
 //Listen to events
-myProgessBar.addEventListener('timeupdate',()=>()
+audioElement.addEventListener('timeupdate', () => {
+    let progress = (audioElement.currentTime / audioElement.duration) * 100;
+    myProgressBar.value = progress;
+});
